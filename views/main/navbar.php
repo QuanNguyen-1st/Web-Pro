b6e544df68267e143299cd8d2d90fce6bb4ae430
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2024-04-29 15:40:02 +0700

    +add bootstrap

--- views/main/navbar.php
+++ views/main/navbar.php
@@ -15,9 +15,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <link rel="stylesheet" href="public/css/style.css">
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="public/css/style.css">
     </head>
     <body>
         <header>
