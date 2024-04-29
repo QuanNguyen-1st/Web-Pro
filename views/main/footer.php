ec8f92d35bb310f2029ac376393c999fce6a91e9
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2024-04-29 14:17:31 +0700

    +Add basic layouts and footer and navbar

---
+++ views/main/footer.php
@@ -0,0 +1,59 @@
+        <section id="news" class="section-p1 section-m1">
+            <div class="text">
+                <h4>Sign Up For Newsletter</h4>
+                <p>Get E-mail updates about our latest shop and <span>special offers</span>.</p>
+            </div>
+            <form>
+                <input type="text" placeholder="Your email address">
+                <button class="normal">Sign Up</button>
+            </form>
+        </section>
+
+        <footer class="section-p1">
+            <div class="col">
+                <img class="logo" src="public/img/logo.png" alt="">
+                <h4>Contact</h4>
+                <p><strong>Address: 268 Ly Thuong Kiet, Ward 14, District 10, Ho Chi Minh City</strong></p>
+                <p><strong>Phone:</strong></p>
+                <p><strong>Hours:</strong></p>
+                <div class="social">
+                    <h4>Follow Us</h4>
+                    <div class="icon">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-pinterest-p"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <h4>About</h4>
+                <a href="#">About Us</a>
+                <a href="#">Delivery Information</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms & Conditions</a>
+                <a href="#">Contact Us</a>
+            </div>
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="#">Sign In</a>
+                <a href="#">View Cart</a>
+                <a href="#">My Wishlist</a>
+                <a href="#">Track My Order</a>
+                <a href="#">Help</a>
+            </div>
+            <div class="col app">
+                <h4>Install App</h4>
+                <p>From App Store or Google Play</p>
+                <div class="row">
+                    <img src="public/img/pay/app.jpg" alt="">
+                    <img src="public/img/pay/play.jpg" alt="">
+                </div>
+                <p>Securd Payment Gateways</p>
+                <img src="public/img/pay/pay.png" alt="">
+            </div>
+        </footer>
+    <script src="public/js/script.js"></script>
+    </body>
+</html>
---
+++ views/main/navbar.php
@@ -0,0 +1,40 @@
+<?php
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }
+    if (isset($_SESSION['guest']))
+    {
+        require_once('models/user.php');
+        $data = User::get($_SESSION['guest']);
+    }
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="public/css/style.css">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    </head>
+    <body>
+        <header>
+            <a href="#"><img src="public/img/logo.png" class="logo" alt=""></a>
+            <nav>
+                <ul id="navbar">
+                    <li><a href = "index.php?page=main&controller=layouts&action=index">Home</a></li>
+                    <li><a href = "index.php?page=main&controller=shop&action=index">Shop</a></li>
+                    <li><a href = "index.php?page=main&controller=blog&action=index">Blog</a></li>
+                    <li><a href = "index.php?page=main&controller=about&action=index">About</a></li>
+                    <li><a href = "index.php?page=main&controller=contact&action=index">Contact</a></li>
+                    <li id="not-mobile-bag"><a href = "#"><i class="far fa-shopping-bag"></i></a></li>
+                    <span id="close-navbar"><i class="far fa-times"></i></span>
+                </ul>
+            </nav>
+            <div id="mobile">
+                <a href = "#"><i class="far fa-shopping-bag"></i></a>
+                <i id="bar" class="fas fa-outdent"></i>
+            </div>
+        </header>
+
