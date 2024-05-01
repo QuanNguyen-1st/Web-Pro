186df0860fbb520a61861488f47760595c2064f3
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2024-05-01 17:12:39 +0700

    *change cdn to local bootstrap

--- views/main/navbar.php
+++ views/main/navbar.php
@@ -17,7 +17,7 @@
         <title>Document</title>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="public/plugins/bootstrap-5.3.3-dist/bootstrap-5.3.3-dist/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="public/css/style.css">
     </head>
     <body>
