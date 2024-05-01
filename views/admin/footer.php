3a4ba2db646f44c2cb4d3d5f2bfddb4bfc19edb5
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2024-05-01 11:06:06 +0700

    *fix footer and add icon header

--- views/admin/footer.php
+++ views/admin/footer.php
@@ -1,7 +1,7 @@
         <footer class="main-footer">
             
             <div class="float-center d-none d-sm-block">
-                <b>Phiên bản</b> 1.0.0<strong></strong> Copyright &copy; 2023
+                <b>Phiên bản</b> 1.0.0<strong></strong> Copyright &copy; 2024
             </div>
 
         </footer>
