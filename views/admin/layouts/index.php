a0040bf165e55c2dec2897bb1f8df606f9679613
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2024-05-04 09:33:04 +0700

    *update

--- views/admin/layouts/index.php
+++ views/admin/layouts/index.php
@@ -1,9 +1,9 @@
-<!-- <?php
+<?php
 session_start();
 if (!isset($_SESSION["user"])) {
 	header("Location: index.php?page=admin&controller=login&action=index");
 }
-?> -->
+?>
 
 <?php
 require_once('views/admin/header.php'); ?>
